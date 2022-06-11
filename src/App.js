00d97ad94ca0c4bbb5331bfeee683487439721ea
theJewelerZ <71835492+theJewelerZ@users.noreply.github.com>
2022-06-11 14:30:11 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect} from "react";
 import './App.css';
+import ContactCard from "./ContactCard";
 
-function App() {
+const App = () => {
+//  const contacts = [
+//    {
+//      avatarUrl: "https://via.placeholder.com/150",
+//      name: "Matt Zarka",
+//      email: "matt.zarka@notreal.com",
+//      age: 49
+//    },
+//    {
+//      avatarUrl: "https://via.placeholder.com/150",
+//      name: "Jerry Norris",
+//      email: "jerry.norris@notreal.com",
+//      age: 50
+//    },
+//    {
+//      avatarUrl: "https://via.placeholder.com/150",
+//      name: "Mark Voldeck",
+//      email: "mark.voldeck@notreal.com",
+//      age: 51
+//    },
+//    {
+//      avatarUrl: "https://via.placeholder.com/150",
+//      name: "Pepo the cat",
+//      email: "pepo.thecat@notreal.com",
+//      age: 5
+//    }
+//  ];
+
+const [results, setResults] = useState([]);
+
+useEffect(()=>{
+  fetch("https://randomuser.me/api/?results=5")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      setResults(data.results)
+    });
+}, []) 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Join <code>The Fledge</code> Dao
-        </p>
-        <a
-          className="App-link"
-          href="https://snapshot.org/#/thefledge.eth"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn How
-        </a>
-      </header>
+    <div>
+      {results.map((result, index)=> {
+        return(
+          <ContactCard key={index}
+            avatarUrl = {result.picture.large} 
+            name = {result.name.first} 
+            email = {result.email} 
+            age = {result.dob.age}/>
+        )
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
